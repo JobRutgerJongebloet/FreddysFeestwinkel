95d5f640d7f97713121d4dc9a43b8486622927fd
T.C. Monpellier <thiery@MacBook-Air-van-TC.local>
2022-12-30 15:37:03 +0100

    product toevoegen knop

---
+++ aanmakenProduct.js
@@ -0,0 +1,53 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+// functie aanmaken 
+function maakProductAan(evt) {
+  evt.preventDefault();
+  let formulier = document.getElementById("product-form")
+  if (formulier.classList.contains('was-validated')) {
+
+    // Formulier uitlezen
+    let NaamInvoer = document.getElementById('productNaam').value;
+    let BeschrijvingInvoer = document.getElementById('productBeschrijving').value;
+    let CategorieInvoer = document.getElementById('productCategorie').value;
+    let PrijsInvoer = document.getElementById('productKosten').value;
+
+    // Javascript object
+    let nieuwProduct = {
+      naam: NaamInvoer,
+      beschrijving: BeschrijvingInvoer,
+      categorie: CategorieInvoer,
+      kosten: PrijsInvoer
+    }
+
+    fetch("http://localhost:8080/product/aanmaken", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(nieuwProduct)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      });
+  }
+}
+
