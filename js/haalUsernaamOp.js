0dcb6c553119c8f758c69d82538a4e2d36f729ef
T.C. Monpellier <thiery@MacBook-Air-van-TC.local>
2023-01-04 13:04:26 +0100

    p

--- js/haalUsernaamOp.js
+++ js/haalUsernaamOp.js
@@ -1,9 +1,28 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      form.classList.add('was-validated1')
+    }, false)
+  })
+})()
+
 
 // functie aanmaken 
 function haalUsernaamOp(evt) {
   evt.preventDefault();
   let formulier = document.getElementById("product-form1")
-  if (formulier.classList.contains('was-validated')) {
+  if (formulier.classList.contains('was-validated1')) {
 
     // Formulier uitlezen
     let usernaamInvoer = document.getElementById('usernaam').value;
