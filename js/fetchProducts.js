7b65888146bf0be2271e5664106b2e044fdfff97
Aman Ahmed <aman16.aa17@gmail.com>
2023-01-04 20:27:59 +0100

    Formatted fetchProducts.js

--- js/fetchProducts.js
+++ js/fetchProducts.js
@@ -12,9 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
                     <div class="filter-card-item p-4 bg-light rounded">
                         <div class="filter-card-img rounded">
                             <img src="img/items/christmas-sweater.jpg" class="item-img" alt="">
-                                <div class="filter-card-heart">
-                                    <img src="img/filter-icons/favourite.png" alt="favourite">
-                                </div>
+                            <div class="filter-card-heart">
+                                <img src="img/filter-icons/favourite.png" alt="favourite">
+                            </div>
                         </div>
                         <div class="filter-card-title">${response[i].naam}</div>
                         <div class="filter-card-description">${response[i].omschrijving}</div>
