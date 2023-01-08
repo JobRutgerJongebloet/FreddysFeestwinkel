d8974e85a67991162299254b773a911b3d37077c
Aman Ahmed <aman16.aa17@gmail.com>
2023-01-08 13:40:55 +0100

    Testing

---
+++ model/Klant.js
@@ -0,0 +1,18 @@
+export class Klant {
+    constructor() {
+        this.naam = naam;
+        this.adres = adres;
+        this.geboortedatum = geboortedatum;
+        this.telefoonnummer = telefoonnummer;
+        this.email = email;
+        this.password = password;
+        this.CCinfo = CCinfo;
+        this.shippingInfo = shippingInfo;
+        this.accountBalance = accountBalance;
+        this.bestelGeschiedenis = bestelGeschiedenis;
+    }
+
+    registerUser() {
+
+    }
+}
