0e0259404d94bba45babb2932e45752a534e6234
niygon <gonenniyazi1993@gmail.com>
2023-01-10 19:04:28 +0100

    winkelwagen

--- js/ophalenProducten.js
+++ js/ophalenProducten.js
@@ -1,13 +1,17 @@
-
-let producten = document.getElementById('product');
-
+haalProductenOp();
+// link.addEventListener("click", function(evt){
+// evt.preventDefault(); 
+// console.log("hoi");
+// });
+let winkelwagen = {};
+let winkelwagenProucten = [];
 function haalProductenOp() {
     fetch(baseURL + 'producten')
         .then(response => response.json())
         .then(lijst => {
-
+            console.log(lijst);
+            let producten = document.getElementById("product");
             for (let d of lijst) {
-                console.log(d);
                 producten.innerHTML += `
                         <div class="col">
                                 <div class="card" style="width: 18rem;">
@@ -17,18 +21,48 @@ function haalProductenOp() {
                                      ${d.naam}            
                                     </h5>
                                     <p class="card-text">
-                                     ${d.beschrijving}
+                                     ${d.omschrijving}
                                     </p>
                                     </div>
                                     <ul class="list-group list-group-flush">
                                         <li class="list-group-item">Categorie: ${d.categorie}</li>
-                                        <li class="list-group-item">Voorraad: ${d.voorraad}</li>
+                                        <li class="list-group-item">Prijs (€): ${d.subtotal}</li>
                                     </ul>
                                     <div class="card body">
-                                    <a href="#" class="btn btn-primary">Toevoegen aan winkelwagen</a>
+                                    <a data-param="${d.id}" id="addtocart" class="btn btn-primary">Toevoegen aan winkelwagen</a>
                                     </div>
                                 </div>
                         </div>`
             }
+            const links = document.querySelectorAll("#addtocart");
+            links.forEach(function (link) {
+                link.addEventListener("click", function (e) {
+                    e.preventDefault();
+                    const productid = this.getAttribute("data-param");
+                    toevoegenAanWinkelwagen(productid);
+
+                });
+
+            });
         })
 }
+function toevoegenAanWinkelwagen(productid) {
+    fetch(baseURL + 'product/' + productid)
+        .then(response => response.json())
+        .then(product => {
+            console.log(product.naam + " " + "is toegevoegd aan winkelwagen");
+            if (winkelwagen.hasOwnProperty(product.naam)) {
+                winkelwagen[product.naam] += 1;
+            } else {
+                winkelwagen[product.naam] = 1;
+            }
+            console.log(winkelwagen);
+            let inhoudww = document.getElementById("inhoudww");
+            let winkelwagenInhoud = '';
+            for (let key in winkelwagen) {
+                winkelwagenInhoud += `${winkelwagen[key]} x ${key} <br>`
+            }
+            inhoudww.innerHTML = winkelwagenInhoud;
+        })
+}
+
