d7fc6fe591f3370256e6ff48ac17498b0b4bf498
Aman Ahmed <aman16.aa17@gmail.com>
2023-01-10 13:53:31 +0100

    Added afbeelding to fetchProducts

--- js/fetchProducts.js
+++ js/fetchProducts.js
@@ -7,20 +7,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Product
         for (let i = 0; i < response.length; i++) {
+            // console.log(response);
+
+            if (response[i].afbeelding != null) {
+                afbeelding = response[i].afbeelding;
+            } else {
+                afbeelding = "img/items/christmas-sweater.jpg";
+            }
             products.innerHTML += `
                 <div class="filter-card col mb-3 all ${response[i].categorie}">
                     <div class="filter-card-item p-4 bg-light rounded">
                         <div class="filter-card-img rounded">
-                            <img src="img/items/christmas-sweater.jpg" class="item-img" alt="">
+                            <img src="${afbeelding}" class="item-img" alt="">
                             <div class="filter-card-heart">
                                 <img src="img/filter-icons/favourite.png" alt="favourite">
                             </div>
                         </div>
                         <div class="filter-card-title">${response[i].naam}</div>
                         <div class="filter-card-description">${response[i].beschrijving}</div>
-                        <div class="filter-card-price">€ 25</div>
+                        <div class="filter-card-price">€ ${response[i].kosten}</div>
                     </div>
                 </div>`;
+
         }
     };
     fetchProducts(baseURL + 'producten')
