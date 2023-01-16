dc162b3ad985a865618d5738aa610c06d1a16dc3
T.C. Monpellier <thiery@MacBook-Air-van-TC.local>
2023-01-16 12:12:18 +0100

    bezig

--- js/haalUsernaamOp.js
+++ js/haalUsernaamOp.js
@@ -1,25 +1,23 @@
 // functie aanmaken 
-test123();
-// async function haalUsernaamOp(evt) {
-//   evt.preventDefault();
-//   let formulier = document.getElementById("product-form1")
+// test123();
+// async function haalUsernaamOp() {
+  // evt.preventDefault();
+  // let formulier = document.getElementById("product-form1")
 
-//   if (!formulier.checkValidity()) {
-//     evt.preventDefault()
-//     evt.stopPropagation()
-//   }
-//   formulier.classList.add('was-validated')
+  // if (!formulier.checkValidity()) {
+  //   evt.preventDefault()
+  //   evt.stopPropagation()
+  // }
+  // formulier.classList.add('was-validated')
 
-//   if (formulier.classList.contains('was-validated')) {
-//   let email = document.getElementById("gebruikersnaam").value;
-//   let password = document.getElementById("password").value;
-//   }
-
-//   checkGebruiker(email, password);
+  // if (formulier.classList.contains('was-validated')) {
+  // let email = document.getElementById("gebruikersnaam").value;
+  // let password = document.getElementById("wachtwoord").value;
+  // }
 
 //   const checkGebruiker = async (email, password) => {
 //     try {
-//       const response = await fetch("http://localhost:8080/klanten/aanmaken", {
+//       const response = await fetch("http://localhost:8080/klanten/inloggen", {
 //         method: 'POST',
 //         headers: {
 //           'Content-Type': 'application/json',
@@ -43,14 +41,38 @@ test123();
 //   };
 
 // }
-function test123(){
+
+function maakAccountAan() {
+  var username = document.getElementById('email').value;
+  var password = document.getElementById('wachtwoord').value;
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+      username: username,
+      password: password
+  });
+
+  console.log(raw);
+
   var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
   };
-  
-  fetch("http://localhost:8080/klanten/email/123@hotmail.com\n", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+
+  fetch("http://localhost:8080/klanten/inloggen", requestOptions)
+      .then(response => response.json())
+      .then(r => {
+
+          if (!!r) {
+              console.log(`Welcome ${email}!`)
+          } else {
+              console.log(`Email of wachtwoord is incorrect!`);
+          }
+      })
+      .catch(error => console.log('error', error));
 }
+
