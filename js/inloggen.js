32417c3a738d840b55d2e0298ebbe24cd5da4a33
T.C. Monpellier <thiery@MacBook-Air-van-TC.local>
2023-01-16 15:23:51 +0100

    k

--- js/inloggen.js
+++ js/inloggen.js
@@ -1,6 +1,54 @@
-let voornaam = document.getElementById('voorNaam');
+import { NavBar } from '../model/navBar.js'
+
+var navbar = new NavBar();
+const formElement = document.getElementById('form');
+// console.log(formElement);
+
+formElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    // formElements.forEach(element => {
+    //     checkValidity(element);
+    // });  
+
+    maakAccountAan()
+});
 
 function maakAccountAan() {
-    console.log(" voornaam: " + voornaam.value)
+    var username = document.getElementById('gebruikersnaam').value;
+    var password = document.getElementById('wachtwoord').value;
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        email: username,
+        password: password
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:8080/klanten/inloggen", requestOptions)
+        .then(response => response.json())
+        .then(r => {
+
+            if (r.validaties == null) {
+                console.log("U bent ingelogd!")
+                localStorage.setItem("response", JSON.stringify(r));
+                navbar.showUsername();
+            } else {
+                console.log("Gebruikersnaam of password is incorrect!");
+                r.validaties.forEach(validatie => {
+                    alert(validatie);
+                });
+            }
+        })
+        .catch(error => console.log('error', error));
 }
+
 
